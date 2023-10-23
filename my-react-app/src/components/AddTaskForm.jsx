9b8e07dcc31f1329bdcd62b3ff13241755cd9201
Fosterosei07 <oprempehfoster@gmail.com>
2023-10-23 12:53:23 +0000

    update: modifications on the css.

---
+++ my-react-app/src/components/AddTaskForm.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function AddTaskForm({ addTask }) {
+  const [task, setTask] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim()) {
+      addTask(task);
+      setTask('');
+    }
+  };
+
+  return (
+    <form id='input' onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Add a new task..."
+      />
+      <button id='btn' type="submit">Add Task</button>
+    </form>
+  );
+}
+
+export default AddTaskForm;
